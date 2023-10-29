826f3988cdde36f8cdfd7865077d59f816bdc02b
HeshanKarunaratne <heshan947102@gmail.com>
2023-10-29 14:33:15 +0530

    react-hook-form and yup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Menu } from './pages/Menu';
-import { createContext } from 'react';
-import { QueryClient, QueryClientProvider, } from '@tanstack/react-query';
-export const AppContext = createContext();
+import { Form } from './pages/Form';
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false
-      }
-    }
-  });
+
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient} >
-        <Router>
-          <div>
-            <Link to="/" >Home</Link>
-          </div>
-          <div>
-            <Link to="/menu" >Menu</Link>
-          </div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
-          </Routes>
-        </Router>
-
-      </QueryClientProvider>
+      <Form />
     </div >
   );
 }
